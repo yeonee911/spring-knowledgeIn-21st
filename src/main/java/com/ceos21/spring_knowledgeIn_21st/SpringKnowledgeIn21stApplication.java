177153a7970bb5712cc558accca34b60ac1c55ee
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-03-27 01:02:23 +0900

    feat: BaseEntity 구현

--- src/main/java/com/ceos21/spring_knowledgeIn_21st/SpringKnowledgeIn21stApplication.java
+++ src/main/java/com/ceos21/spring_knowledgeIn_21st/SpringKnowledgeIn21stApplication.java
@@ -2,7 +2,9 @@ package com.ceos21.spring_knowledgeIn_21st;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringKnowledgeIn21stApplication {
 
