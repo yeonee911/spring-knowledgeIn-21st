fbf635512913e39d00b75f8c7a5ec76da9480724
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-04-29 18:14:30 +0900

    feat:; 댓글 수정 연관관계 메서드

--- src/main/java/com/ceos21/spring_knowledgeIn_21st/domain/comment/domain/AnswerComment.java
+++ src/main/java/com/ceos21/spring_knowledgeIn_21st/domain/comment/domain/AnswerComment.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,5 +25,9 @@ public class AnswerComment extends BaseComment {
 
     public void setAnswer(Answer answer) {
         this.answer = answer;
+    }
+
+    public void update(@NotBlank String content) {
+        this.content = content;
     }
 }
